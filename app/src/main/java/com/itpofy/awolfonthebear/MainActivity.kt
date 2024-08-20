0748d5bcf11ae48adc608700d25a7e7a1d8617d5
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-08-20 22:39:25 +0800

    jasd

--- app/src/main/java/com/itpofy/awolfonthebear/MainActivity.kt
+++ app/src/main/java/com/itpofy/awolfonthebear/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AwolfonthebearTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Friend",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
